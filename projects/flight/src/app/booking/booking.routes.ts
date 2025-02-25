c6a0155373951c5cb3c501b3cc38e481f0b65b5f
mikezks <23002661+mikezks@users.noreply.github.com>
2025-02-25 13:14:55 +0100

    refactor: fn resolver, router cmp input binding

--- projects/flight/src/app/booking/booking.routes.ts
+++ projects/flight/src/app/booking/booking.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from "@angular/router";
 import { provideEffects } from "@ngrx/effects";
 import { provideState } from "@ngrx/store";
+import { FlightBookingComponent, FlightEditComponent, FlightSearchComponent } from "./feature-flight";
 import { TicketEffects } from "./logic-flight/+state/effects";
 import { ticketFeature } from "./logic-flight/+state/reducer";
-import { FlightSearchComponent, FlightEditComponent, FlightBookingComponent } from "./feature-flight";
-import { FlightResolver } from "./logic-flight/data-access/flight.resolver";
+import { resolveFlight } from "./logic-flight/data-access/flight.resolver";
 
 
 export const BOOKING_ROUTES: Routes = [
@@ -37,7 +37,7 @@ export const BOOKING_ROUTES: Routes = [
             path: 'edit/:id',
             component: FlightEditComponent,
             resolve: {
-              flight: FlightResolver
+              flight: resolveFlight
             }
           }
         ]
