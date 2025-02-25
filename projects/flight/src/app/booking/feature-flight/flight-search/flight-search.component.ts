3e8aeb3c673f95cffe609ebfc4784eeb97e52ab2
mikezks <23002661+mikezks@users.noreply.github.com>
2025-02-25 10:33:28 +0100

    refactor: smart cmp to standalone flight search

--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -1,11 +1,19 @@
+import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { Flight, FlightFilter } from '../../logic-flight';
+import { FlightCardComponent } from '../../ui-flight/flight-card/flight-card.component';
+import { UiFlightModule } from '../../ui-flight/ui-flight.module';
 import { TicketsFacade } from './../../logic-flight/+state/facade';
 
 
 @Component({
   selector: 'app-flight-search',
-  standalone: false,
+  // standalone: true,
+  imports: [
+    NgIf, NgFor, JsonPipe, AsyncPipe,
+    UiFlightModule,
+    FlightCardComponent
+  ],
   templateUrl: './flight-search.component.html',
 })
 export class FlightSearchComponent {
